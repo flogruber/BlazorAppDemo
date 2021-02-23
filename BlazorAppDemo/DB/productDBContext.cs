3427cda7dc5b1a484775dbcdc78e938c90eb07d0
Flo Gruber <49080575+flogruber@users.noreply.github.com>
2021-02-23 17:47:08 +0100

    Projektdateien hinzufügen.

---
+++ BlazorAppDemo/DB/productDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorAppDemo.Models;
+
+namespace BlazorAppDemo.DB
+{
+    public class productDBContext : DbContext
+    {
+        public productDBContext(DbContextOptions<productDBContext> options): base(options)
+        {
+            // Nothing to do
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Nothing to do
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DevProduct>()
+                .ToContainer("devProducts");
+
+            modelBuilder.Entity<DevProduct>()
+                .HasKey(x => x.productID);
+
+            modelBuilder.Entity<DevProduct>()
+                .OwnsMany(x => x.Comments);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<DevProduct> Products { get; set; }
+        public DbSet<DevProductComments> Comments { get; set; }
+    }
+}
---
+++ BlazorAppDemo/Models/DevProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppDemo.Models
+{
+    public class DevProduct
+    {
+        [Key]
+        public string productID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public ICollection<DevProductComments> Comments { get; set; }
+
+        public DevProduct()
+        {
+            productID = Guid.NewGuid().ToString();
+        }
+    }
+
+    public class DevProductComments
+    {
+        [Key]
+        public string commentID { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public bool Approved { get; set; }
+
+        public DevProductComments()
+        {
+            commentID = Guid.NewGuid().ToString();
+        }
+    }
+}
